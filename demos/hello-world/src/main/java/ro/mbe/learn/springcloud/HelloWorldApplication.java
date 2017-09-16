5c6f4b7b8c8d22079e05d3bdd18470cb243a5ca1
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-09-16 12:21:28 +0200

    Reconfigured as gradle multiple projects, removed maven

---
+++ demos/hello-world/src/main/java/ro/mbe/learn/springcloud/HelloWorldApplication.java
@@ -0,0 +1,21 @@
+package ro.mbe.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String sayHello() {
+        return "Hello from String Boot!";
+    }
+}
