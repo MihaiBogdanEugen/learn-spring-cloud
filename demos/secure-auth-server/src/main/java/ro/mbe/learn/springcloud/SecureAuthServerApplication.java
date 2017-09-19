d5c89724183c514e843b87def40957484febe851
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-09-19 18:16:20 +0200

    Added stub projects for securing microservices demos.

---
+++ demos/secure-auth-server/src/main/java/ro/mbe/learn/springcloud/SecureAuthServerApplication.java
@@ -0,0 +1,21 @@
+package ro.mbe.learn.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SecureAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureAuthServerApplication.class, args);
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "Secure Auth Server";
+    }
+}
