91dc207c59f09dbe719c82ec647ff0b2372598e0
Bogdan Mihai <mihai.bogdan.eugen@gmail.com>
2017-09-21 13:54:02 +0200

    Updated Spring Cloud Task demos

--- demos/task-sink-rabbitmq/src/main/java/ro/mbe/learn/springcloud/TaskSinkRabbitMQApplication.java
+++ demos/task-sink-rabbitmq/src/main/java/ro/mbe/learn/springcloud/TaskSinkRabbitMQApplication.java
@@ -3,21 +3,12 @@ package ro.mbe.learn.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.task.launcher.annotation.EnableTaskLauncher;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableTaskLauncher
 public class TaskSinkRabbitMQApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskSinkRabbitMQApplication.class, args);
     }
-
-//    @RequestMapping(value = "/", method = RequestMethod.GET)
-//    public String home() {
-//        return "Task Sink RabbitMQ";
-//    }
 }
